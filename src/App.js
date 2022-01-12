6e599c29cc3f8dbc20ec2b087cc0ca85591b76b1
Celdama <celdama.dev@gmail.com>
2022-01-12 10:51:49 +0100

    setup start for this project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,67 @@
-function App() {
-  return <div className='App'>note app</div>;
-}
+import { useState } from 'react';
+import { data } from './data';
+import Split from 'react-split';
+import { nanoid } from 'nanoid';
+import Sidebar from './components/Sidebar';
+import Editor from './components/Editor.js';
+
+const App = () => {
+  const [notes, setNotes] = useState([]);
+  const [currentNoteId, setCurrentNoteId] = useState(
+    (notes[0] && notes[0].id) || ''
+  );
+
+  const createNewNote = () => {
+    const newNote = {
+      id: nanoid(),
+      body: "# Type your markdown note's title here",
+    };
+    setNotes((prevNotes) => [newNote, ...prevNotes]);
+    setCurrentNoteId(newNote.id);
+  };
+
+  const updateNote = (text) => {
+    setNotes((oldNotes) =>
+      oldNotes.map((oldNote) => {
+        return oldNote.id === currentNoteId
+          ? { ...oldNote, body: text }
+          : oldNote;
+      })
+    );
+  };
+
+  const findCurrentNote = () => {
+    return (
+      notes.finds((note) => {
+        return note.id === currentNoteId;
+      }) || notes[0]
+    );
+  };
+
+  return (
+    <main>
+      {notes.length > 0 ? (
+        <Split sizes={(30, 70)} direction='horizontal' className='split'>
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+          />
+          {currentNoteId && notes.length > 0 && (
+            <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+          )}
+        </Split>
+      ) : (
+        <div className='no-notes'>
+          <h1>you have no note</h1>
+          <button className='first-note' onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )}
+    </main>
+  );
+};
 
 export default App;
